6481cebf1c5f723f3c3dfc9204572a2c7be34fb8
121AzizAbdul <abdulaziz29ru@gmail.com>
2023-02-15 01:28:00 -0500

    some change

--- Sublime/main.cpp
+++ Sublime/main.cpp
@@ -12,12 +12,31 @@ using namespace std;
 const ll sz=2e5+7,Inf=1e9+7;
 
 void solve(){ 
- int a,b;
- cin>>a>>b;
- cout<<a+b;
+ map<char,int>w;
+ map<char,int>b;
+ w['Q']=9;w['R']=5;w['B']=3;w['N']=3;w['P']=1;
+ b['q']=9;b['r']=5;b['b']=3;b['n']=3;b['p']=1;
+ int ws=0,bs=0;
+ for(int i=0;i<8;i++){
+ 	for(int j=0;j<8;j++){
+ 		char x;
+ 		cin>>x;
+ 		if(w[x]>0)ws+=w[x];
+ 		else if(b[x]>0)bs+=b[x];
+
+ 	}
+ }
+ if(ws==bs) cout<<"Draw"<<nl;
+ else if(ws>bs) cout<<"White";
+ else cout<<"Black";
 }
 int main() {
+	#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
   fast;
-   solve();
+  //TC
+  solve();
   return 0;
 }
