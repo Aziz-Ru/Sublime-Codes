129a90e0918e68f8ec37f3544775ce2484622eb0
121AzizAbdul <abdulaziz29ru@gmail.com>
2023-03-16 08:01:55 -0400

    c++ file added

---
+++ Sublime/Zahin/Code/main.cpp
@@ -0,0 +1,24 @@
+using namespace std;
+#define TC int t; cin>>t;while(t--)
+#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+#define nl '\n'
+#define all(x) (x).begin(),(x).end()
+#define pb push_back
+#define ff first 
+#define ss second 
+#define debug(x) cout<<#x<<" "<<x<<nl;
+const ll sz=2e5+7,Inf=1e9+7;
+void solve(){
+	
+}
+int main() {
+  fast;
+  #ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+  #endif
+  TC
+  solve();
+  return 0;
+}
